e4754059b769695507736d91b575e2f439226267
joaquin <joaquin.ecker@gmail.com>
2022-02-05 20:00:25 -0300

    Functionality tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 type formElement = React.FormEvent<HTMLFormElement>;
 interface ITask {
@@ -9,16 +9,30 @@ interface ITask {
 function App(): JSX.Element {
   const [newTask, setNewTask] = useState<string>("");
   const [tasks, setTasks] = useState<ITask[]>([]);
+  const taskInput = useRef<HTMLInputElement>(null);
 
   const handleSubmit = (e: formElement) => {
     e.preventDefault();
     addTask(newTask);
     setNewTask("");
+    taskInput.current?.focus();
   };
 
-  const addTask = (name: string) => {
+  const addTask = (name: string): void => {
     const newTasks: ITask[] = [...tasks, { name: name, done: false }];
     setTasks(newTasks);
+  };
+
+  const changeState = (i: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[i].done = !newTasks[i].done;
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (i: number): void => {
+    const tasksList: ITask[] = [...tasks];
+    tasksList.splice(i, 1);
+    setTasks(tasksList);
   };
 
   return (
@@ -33,9 +47,15 @@ function App(): JSX.Element {
                   type="text"
                   onChange={(e) => setNewTask(e.target.value)}
                   value={newTask}
+                  ref={taskInput}
                   autoFocus
                 />
-                <button className="btn btn-success mt-2">Save</button>
+                <button
+                  className="btn btn-success mt-2"
+                  style={{ width: "100%" }}
+                >
+                  Save
+                </button>
               </form>
             </div>
           </div>
@@ -43,8 +63,20 @@ function App(): JSX.Element {
       </div>
       {tasks.map((task: ITask, i: number) => (
         <div className="card card-body mt-2" key={i}>
-          <h2>{task.name}</h2>
-          <p>{task.done}</p>
+          <h2 style={{ textDecoration: task.done ? "line-through" : "" }}>
+            {task.name}
+          </h2>
+          <div>
+            <button
+              className="btn btn-secondary"
+              onClick={() => changeState(i)}
+            >
+              {task.done ? "✅" : "✗"}
+            </button>
+            <button className="btn btn-danger" onClick={() => deleteTask(i)}>
+              🗑️
+            </button>
+          </div>
         </div>
       ))}
     </div>
